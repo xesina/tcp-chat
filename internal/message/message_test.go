d13ba31b139ba2348091add2a9fb254214bb38d6
Sina Saeidi <xesina@gmail.com>
2019-08-27 02:52:52 +0430

    add test for message.Read method

--- internal/message/message_test.go
+++ internal/message/message_test.go
@@ -1,9 +1,47 @@
 package message
 
 import (
+	"bufio"
+	"bytes"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"testing"
 )
+
+func TestRead(t *testing.T) {
+	tt := []struct {
+		given *bufio.Reader
+		want  string
+		err   error
+	}{
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("identity\n"))),
+			want:  IdentityMsg,
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("IDENTITY\n"))),
+			want:  IdentityMsg,
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("TEST"))),
+			want:  "",
+			err:   io.EOF,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("\n"))),
+			want:  "",
+			err:   nil,
+		},
+	}
+
+	for _, tc := range tt {
+		msg, err := Read(tc.given)
+		assert.Equal(t, tc.err, err)
+		assert.Equal(t, tc.want, msg)
+	}
+}
 
 func TestJoinRecipients(t *testing.T) {
 	tt := []struct {
