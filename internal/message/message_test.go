8c45f7340a0c04e4aa147d6fe01ecc88bc748fef
Sina Saeidi <xesina@gmail.com>
2019-08-27 03:02:39 +0430

    add tests for ReadBytesArg and ReadStringArg

--- internal/message/message_test.go
+++ internal/message/message_test.go
@@ -43,6 +43,96 @@ func TestRead(t *testing.T) {
 	}
 }
 
+func TestReadStringArg(t *testing.T) {
+	tt := []struct {
+		given *bufio.Reader
+		want  string
+		err   error
+	}{
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("arg1\n"))),
+			want:  "arg1",
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("ARG1\n"))),
+			want:  "ARG1",
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("ARG1    \n"))),
+			want:  "ARG1",
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("1,2,3,4,5\n"))),
+			want:  "1,2,3,4,5",
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("TEST"))),
+			want:  "",
+			err:   io.EOF,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("\n"))),
+			want:  "",
+			err:   nil,
+		},
+	}
+
+	for _, tc := range tt {
+		msg, err := ReadStringArg(tc.given)
+		assert.Equal(t, tc.err, err)
+		assert.Equal(t, tc.want, msg)
+	}
+}
+
+func TestReadBytesArg(t *testing.T) {
+	tt := []struct {
+		given *bufio.Reader
+		want  []byte
+		err   error
+	}{
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("arg1\n"))),
+			want:  []byte("arg1"),
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("ARG1\n"))),
+			want:  []byte("ARG1"),
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("ARG1    \n"))),
+			want:  []byte("ARG1"),
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("1,2,3,4,5\n"))),
+			want:  []byte("1,2,3,4,5"),
+			err:   nil,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("TEST"))),
+			want:  []byte(""),
+			err:   io.EOF,
+		},
+		{
+			given: bufio.NewReader(bytes.NewBuffer([]byte("\n"))),
+			want:  []byte(nil),
+			err:   nil,
+		},
+	}
+
+	for _, tc := range tt {
+		msg, err := ReadBytesArg(tc.given)
+		assert.Equal(t, tc.err, err)
+		assert.Equal(t, tc.want, msg)
+	}
+}
+
 func TestJoinRecipients(t *testing.T) {
 	tt := []struct {
 		given []uint64
